6ddaacea2b70d412ce86f5230d6b3aa43cd97afd
zaifoski <sofiachimirri@gmail.com>
2017-07-08 18:40:59 +0200

    modify server request 4 to also handle lifestatus, 6

--- src/introsde/rest/ehealth/resources/PersonCollectionResource.java
+++ src/introsde/rest/ehealth/resources/PersonCollectionResource.java
@@ -83,25 +83,31 @@ public class PersonCollectionResource {
     @Consumes(MediaType.APPLICATION_XML)
     public Person newPerson(Person person) throws IOException {        
         if (person.getLifeStatus() != null){
+			Person pe = Person.savePerson(person);
+			pe.setLastname("uuuuuuuuuuuuuuuuuuuuuu");
+			return pe;
+			/*
 			List<LifeStatus> personLifeStatus = new ArrayList<LifeStatus>();
 			personLifeStatus.addAll(person.getLifeStatus());
+			person.setLifeStatus(null);
 			Person p = Person.savePerson(person);
 			int personId = p.getIdPerson();
 			for (int i=0;i<personLifeStatus.size();i++) {
-				LifeStatus ls = personLifeStatus.get(i);
-				MeasureDefinition md = MeasureDefinition.getMeasureDefinition(ls.getMeasureDefinition().getMeasureName());
-				ls.setMeasureDefinition(md);
-				ls.setPerson(p);
-				LifeStatus.saveLifeStatus(ls);
+				LifeStatus lifeS = personLifeStatus.get(i);
+				MeasureDefinition md = MeasureDefinition.getMeasureDefinition(lifeS.getMeasureDefinition().getMeasureName());
+				lifeS.setMeasureDefinition(md);
+				lifeS.setPerson(p);
+				LifeStatus.saveLifeStatus(lifeS);
 				Calendar calendar = Calendar.getInstance();
 				HealthMeasureHistory hm = new HealthMeasureHistory();
 				hm.setMeasureDefinition(md);
 				hm.setPerson(p);
 				hm.setTimestamp(calendar.getTime());
-				hm.setValue(ls.getValue());
+				hm.setValue(lifeS.getValue());
 				HealthMeasureHistory.saveHealthMeasureHistory(hm);
 			}			
 			return Person.getPersonById(personId);
+				*/
         }
         return Person.savePerson(person);
     }
